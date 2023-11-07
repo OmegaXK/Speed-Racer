fb17ba26eb2559bb59ba622cafc9c47b249e2d81
OmegaXK <omegaxk314@gmail.com>
2023-11-06 21:20:59 -0500

    Updated website added credits page

--- main_files/webapp.py
+++ main_files/webapp.py
@@ -4,13 +4,18 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
-    """Speed Racer Home"""
+    """Speed Racer Home."""
     return render_template('home.html')
 
 @app.route('/download')
 def download():
-    """Speed Racer Download"""
+    """Speed Racer Download."""
     return render_template('download.html')
+
+@app.route('/credits')
+def credits():
+    """Speed Racer Credits."""
+    return render_template('credits.html')
 
 if __name__ == '__main__':
     app.run(port=5001)
